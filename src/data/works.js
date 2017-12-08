5359e0746e322b107fa0ac0f78a500f2d1136f48
Igor Egay <egayigor@gmail.com>
2017-12-08 13:24:00 +0500

    Fix work updating

--- src/data/works.js
+++ src/data/works.js
@@ -36,13 +36,14 @@ const addOrEditWork = (args) => {
     return new Promise((resolve, reject) => {
 
         if (args.id) {
-            Works.update({ title: args.title }, { where: { work_id: args.id }})
+            Works.findById(args.id)
                 .then(work => {
+                    work.update({ title: args.title });
                     work.setExercises(args.exercises);
                     work.setPupils(args.pupils);
                     resolve(work)
                 })
-                .catch(error => reject(error));
+                .catch(error => reject(error))
             return;
         }
 
